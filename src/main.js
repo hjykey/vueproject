586aaee0e63cb94039b4ab68bf15cdabe96f19e1
song <276959848@qq.com>
2020-12-24 17:59:09 +0800

    修改home内容

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import './plugins/element.js'
+// 定义了全局样式
 import './assets/css/global.css'
+// 第三方ico
 import './assets/fonts/iconfont.css'
 
 Vue.config.productionTip = false
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// axios请求拦截器，
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // return是固定写法
+  return config
+})
 Vue.prototype.$http = axios
 
 new Vue({
