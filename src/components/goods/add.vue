55b9204c5ac5c6bc4778399190807a2b5555cb4a
jason hau <276959848@qq.com>
2021-01-06 23:35:06 +0800

    P304

--- src/components/goods/add.vue
+++ src/components/goods/add.vue
@@ -17,11 +17,11 @@
         :closable="false"
       >
       </el-alert>
-      <!-- 步骤条 -->
+      <!-- 步骤条 activeIndex-0把字符转为数字-->
       <el-steps
-        :active="activeNum"
+        :active="activeIndex - 0"
         finish-status="success"
-        :space="200"
+        :space="'20%'"
         align-center
         style="margin:1rem;"
       >
@@ -32,6 +32,51 @@
         <el-step title="商品内容"></el-step>
         <el-step title="完成"></el-step>
       </el-steps>
+      <!-- tabs标签页 -->
+      <el-form
+        :model="addForm"
+        :rules="rules"
+        ref="addFormRef"
+        label-width="100px"
+        label-position="top"
+      >
+        <el-tabs
+          v-model="activeIndex"
+          :tab-position="'left'"
+          style="margin:1rem"
+        >
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <el-cascader
+                v-model="selectedItem"
+                :options="cateList"
+                :props="{
+                  expandTrigger: 'hover',
+                  value: 'cat_id',
+                  label: 'cat_name',
+                  children: 'children'
+                }"
+                @change="handleSelectedItemChange"
+              ></el-cascader> </el-form-item
+          ></el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
     </el-card>
   </div>
 </template>
@@ -40,11 +85,46 @@
 export default {
   data () {
     return {
-      activeNum: 1
+      // steps组件的激活索引号
+      activeIndex: '0',
+      addForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0
+      },
+      rules: {
+        goods_name: [
+          { required: true, message: '请输入商品名称', trigger: 'blur' }
+        ],
+        goods_price: [
+          { required: true, message: '请输入商品价钱', trigger: 'blur' }
+        ],
+        goods_weight: [
+          { required: true, message: '请输入商品重量', trigger: 'blur' }
+        ],
+        goods_number: [
+          { required: true, message: '请输入商品数量', trigger: 'blur' }
+        ]
+      },
+      // 商品类型列表
+      cateList: {},
+      // 选中的商品分类
+      selectedItem: ''
     }
   },
-  created () {},
-  methods: {}
+  created () {
+    this.getCateList()
+  },
+  methods: {
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.cateList = res.data
+      console.log(this.cateList)
+    },
+    handleSelectedItemChange () {}
+  }
 }
 </script>
 
